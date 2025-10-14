0ae61542ac5c083d03a48fe4bf407f29d4190e69
JMJ <ghtntkdnsk@naver.com>
2025-10-14 23:40:53 +0900

    feat: 커스텀 구분자가 존재할 경우, 기본 구분자 및 커스텀 구분자로 구분하여 숫자들을 추출

--- src/main/java/calculator/controller/StringCalculator.java
+++ src/main/java/calculator/controller/StringCalculator.java
@@ -1,11 +1,15 @@
 package calculator.controller;
 
+import calculator.domain.BasicDelimiter;
 import calculator.domain.CustomDelimiter;
 import calculator.view.InputView;
 import calculator.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
-    private final static int INDEX_OF_CUSTOM_SPLITER = 2;
+    private final static int INDEX_OF_CUSTOM_DELIMITER = 2;
+    private final static int BEGIN_INDEX_OF_SUBSTRING = 5;
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -16,14 +20,28 @@ public class StringCalculator {
 
     public void start() {
         String string = inputView.readString();
+        List<Integer> numbers = new ArrayList<>();
 
         if (hasCustomSpliter(string)) {
-            CustomDelimiter customDelimiter = new CustomDelimiter(string.charAt(INDEX_OF_CUSTOM_SPLITER));
+            String substring = string.substring(BEGIN_INDEX_OF_SUBSTRING);
+            String[] splitString = splitSubString(substring, string);
+            parseInteger(splitString, numbers);
         }
-
     }
 
     private boolean hasCustomSpliter(String string) {
         return string.startsWith("//") && (string.startsWith("\\n", 3));
+    }
+
+    private static String[] splitSubString(String substring, String string) {
+        return substring.split(BasicDelimiter.COMMA.getDelimiter() + "|"
+                + BasicDelimiter.COLON.getDelimiter() + "|"
+                + CustomDelimiter.createDelimiter(string.charAt(INDEX_OF_CUSTOM_DELIMITER)));
+    }
+
+    private void parseInteger(String[] splitString, List<Integer> numbers) {
+        for (String s : splitString) {
+            numbers.add(Integer.parseInt(s));
+        }
     }
 }
