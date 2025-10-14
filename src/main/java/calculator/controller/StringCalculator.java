860e752a4f6dd5a590801b49fcbbc6571c061343
JMJ <ghtntkdnsk@naver.com>
2025-10-14 21:35:06 +0900

    feat: 덧셈할 문자열을 입력받을 수 있도록 안내 메시지 출력

--- src/main/java/calculator/controller/StringCalculator.java
+++ src/main/java/calculator/controller/StringCalculator.java
@@ -1,4 +1,18 @@
 package calculator.controller;
 
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class StringCalculator {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public StringCalculator(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        String string = inputView.readString();
+    }
 }
