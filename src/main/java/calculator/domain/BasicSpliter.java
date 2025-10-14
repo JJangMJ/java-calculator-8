b08607b0fa289e3a7eec065777efa6c43a18fd6d
JMJ <ghtntkdnsk@naver.com>
2025-10-14 22:10:36 +0900

    feat: 기본 구분자를 Enum 클래스로 생성

--- src/main/java/calculator/domain/BasicSpliter.java
+++ src/main/java/calculator/domain/BasicSpliter.java
@@ -1,4 +1,16 @@
 package calculator.domain;
 
 public enum BasicSpliter {
+    COMMA(","),
+    COLON(":");
+
+    private final String spliter;
+
+    BasicSpliter(String spliter) {
+        this.spliter = spliter;
+    }
+
+    public String getSpliter() {
+        return spliter;
+    }
 }
