d60963e9a8d3791d1e7abc27ad59ec85c50d8f66
JMJ <ghtntkdnsk@naver.com>
2025-10-15 19:23:39 +0900

    fix: 정규표현식을 음수도 포함시키도록 변경하여 음수가 들어왔을 때 예외처리가 제대로 되도록 함

--- src/main/java/calculator/controller/StringCalculator.java
+++ src/main/java/calculator/controller/StringCalculator.java
@@ -56,11 +56,10 @@ public class StringCalculator {
     private List<Integer> parseIntegers(String[] splitString) {
         List<Integer> numbers = new ArrayList<>();
         for (String string : splitString) {
-            String replacedAll = string.replaceAll("^[0-9]*$", "");
+            String replacedAll = string.replaceAll("[^0-9-]", "");
             if (replacedAll.isEmpty()) replacedAll = "0";
             validateNumber(replacedAll);
-            int number = Integer.parseInt(replacedAll);
-            numbers.add(number);
+            numbers.add(Integer.parseInt(replacedAll));
         }
         return numbers;
     }
