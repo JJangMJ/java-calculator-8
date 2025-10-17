80ab06b4f68173d98504ef17458ed4b27d419bac
JMJ <ghtntkdnsk@naver.com>
2025-10-17 21:26:26 +0900

    refactor: 쓸모없는 멤버변수 삭제

--- src/main/java/calculator/domain/Splitter.java
+++ src/main/java/calculator/domain/Splitter.java
@@ -6,19 +6,16 @@ public class Splitter {
     private final static int BEGIN_INDEX_OF_SUBSTRING = 5;
     private static final String COMMA = Pattern.quote(BasicDelimiter.COMMA.getDelimiter());
     private static final String COLON = Pattern.quote(BasicDelimiter.COLON.getDelimiter());
-
     private final String string;
-    private CustomDelimiter customDelimiter;
 
     public Splitter(String string) {
         this.string = string;
     }
 
     public String[] split(CustomDelimiter delimiter) {
-        this.customDelimiter = delimiter;
-        if (customDelimiter != null) {
+        if (delimiter != null) {
             String substring = string.substring(BEGIN_INDEX_OF_SUBSTRING);
-            String custom = Pattern.quote(customDelimiter.getDelimiter());
+            String custom = Pattern.quote(delimiter.getDelimiter());
             return substring.split(COMMA + "|" + COLON + "|" + custom);
         }
         return string.split(COMMA + "|" + COLON);
