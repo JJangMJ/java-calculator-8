680fcbf5ad118a981621a5376f44475d94a3930d
JMJ <ghtntkdnsk@naver.com>
2025-10-19 17:22:04 +0900

    test: 커스텀 구분자 관련 예외 처리 테스트

---
+++ src/test/java/calculator/domain/CustomDelimiterTest.java
@@ -0,0 +1,28 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import calculator.exception.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+public class CustomDelimiterTest {
+    @Test
+    void 숫자는_커스텀_구분자로_쓸_수_없다() {
+        assertThatThrownBy(() -> new CustomDelimiter('1'))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_CUSTOM_DELIMITER.getErrorMessage());
+    }
+
+    @Test
+    void 공백은_커스텀_구분자로_쓸_수_없다() {
+        assertThatThrownBy(() -> new CustomDelimiter(' '))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_CUSTOM_DELIMITER.getErrorMessage());
+    }
+
+    @Test
+    void 일반_문자는_허용된다() {
+        new CustomDelimiter(';');
+        new CustomDelimiter('#');
+    }
+}
